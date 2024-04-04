ba1558400b357371484e5fbadde355baf3484a8f
Santiago <torrisantiago@gmail.com>
2024-04-04 20:32:47 -0300

    App.jsx linked to stylesheet and nav-bar component created

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <div className="nav-bar">
+      <div>Item 1</div>
+      <div>Item 2</div>
+      <div>Item 3</div>
+      <div>Item 4</div>
+    </div>
+  );
+}
+
+export default NavBar;
